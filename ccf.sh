a657d564f375cf2344d32e58e0b79d98c12bc83f
Karl Ole Voss <ole.voss@posteo.de>
2021-01-20 14:35:21 +0100

    add deploy script

---
+++ ccf.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# setup
+set -e
+
+# get filename
+file=$(basename $1 .c)
+
+# compile .c to .elf
+avr-gcc -mmcu=atmega328p -Wall -Os -o $file.elf $file.c
+
+# copy elf sections
+avr-objcopy -j .text -j .data -O ihex $file.elf $file.hex
+
+# flash to atmega328p
+avrdude -p m328p -c usbasp -e -U flash:w:$file.hex
+
+# clean temorary files
+rm $file.elf $file.hex
