b79f5d43b7c82d2cfcb5d7c17a02671e97c7166a
Karl Ole Voss <ole.voss@posteo.de>
2021-01-20 15:38:48 +0100

    led btn test

--- components/station/tests/led_btn.c
+++ components/station/tests/led_btn.c
@@ -2,5 +2,13 @@
 
 int main (void) {
 	DDRB |= (1 << DDB0);
-	PORTB |= (1 << DDB0);
+	PORTB |= (1 << PB1); // pull-up resistor
+	
+	while (1) {
+		if (!(PINB & 0b00000010)) {
+			PORTB |= (1 << PB0);
+		} else {
+			PORTB &= ~(1 << PB0);
+		}
+	}
 }
