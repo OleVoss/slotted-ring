f9bd20d0b4a41a734ebb0d08b9c940184df633ac
Karl Ole Voß <ole.voss@posteo.de>
2021-01-22 20:12:07 +0100

    fix ccf

--- ccf.sh
+++ ccf.sh
@@ -2,12 +2,13 @@
 
 # setup
 set -e
+lib_path=/home/ole/Repos/slotted-ring/components/avr_lib/src
 
 # get filename
 file=$(basename $1 .c)
 
 # compile .c to .elf
-avr-gcc -mmcu=atmega328p -Wall -Os -o $file.elf $file.c
+avr-gcc -mmcu=atmega328p -Wall -Os -o $file.elf $file.c $lib_path/lcdpcf8574/lcdpcf8574.c $lib_path/i2chw/twimaster.c $lib_path/pcf8574/pcf8574.c
 
 # copy elf sections
 avr-objcopy -j .text -j .data -O ihex $file.elf $file.hex
