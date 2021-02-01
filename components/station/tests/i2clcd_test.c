4787088c0f39aefee9d83de960febadc5c86e684
Karl Ole Voss <ole.voss@posteo.de>
2021-02-01 12:41:51 +0100

    funkt

--- components/station/tests/i2clcd_test.c
+++ components/station/tests/i2clcd_test.c
@@ -4,14 +4,14 @@
 #include <avr/pgmspace.h>
 #include <avr/interrupt.h>
 
-#include "/home/voss/repos/slotted-ring/components/libs/src/lcdpcf8574/lcdpcf8574.h"
-#include "/home/voss/repos/slotted-ring/components/libs/src/i2chw/i2cmaster.h"
+#include "/home/voss/repos/slotted-ring/components/avr_lib/src/lcdpcf8574/lcdpcf8574.h"
+#include "/home/voss/repos/slotted-ring/components/avr_lib/src/i2chw/i2cmaster.h"
 
 int main(void) {
 	i2c_init();
 
 	lcd_init(LCD_DISP_ON);
-	
+	lcd_home();	
 	lcd_puts("Station Nr: 1");
 }
 
